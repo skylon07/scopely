d23a60830dce6cef7b4bccfc96c33799f49f205c
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2025-06-06 03:17:13 -0600

    Initialize dart package

---
+++ lib/async_scope.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/async_scope_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
