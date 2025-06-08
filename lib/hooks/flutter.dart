4ee88cc51621528ef66ca13998774d2cb7f534ad
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2025-06-07 18:03:53 -0600

    Ignore all errors in the file

--- lib/hooks/flutter.dart
+++ lib/hooks/flutter.dart
@@ -1,3 +1,10 @@
+// ignore_for_file: depend_on_referenced_packages
+// ignore_for_file: uri_does_not_exist
+// ignore_for_file: undefined_class
+// ignore_for_file: mixin_super_class_constraint_non_interface
+// ignore_for_file: undefined_super_member
+// ignore_for_file: override_on_non_overriding_member
+
 import 'package:async_scope/async_scope.dart';
 import 'package:flutter/widgets.dart';
 
