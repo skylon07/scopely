ad34f313ea8dcf52ed48bf4edc652b064eddd1a1
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2025-06-11 04:31:05 -0600

    Add stream merging functions

---
+++ lib/src/merge_streams.dart
@@ -0,0 +1,259 @@
+import 'dart:async';
+
+import 'package:scopely/scopely.dart';
+
+Stream<List<dynamic>> mergeStreams(List<Stream<dynamic>> streams) {
+  var manager = _MergeStreamsManager();
+  for (var (idx, stream) in streams.indexed) {
+    // the resulting stream (which is always just the shared controller's stream)
+    // can be ignored; this transformation is just for the side effects
+    stream.cast<dynamic>().transform(_MergeStreamsTransformer<dynamic>(manager, streamIdx: idx, originalStream: stream));
+  }
+  return manager.sharedDestController.stream;
+}
+
+Stream<(E1, E2)> mergeStreams2<E1, E2>(
+  Stream<E1> stream1, 
+  Stream<E2> stream2,
+) {
+  return mergeStreams([stream1, stream2]).map((list) {
+    var [event1, event2] = list;
+    return (event1, event2);
+  });
+}
+
+Stream<(E1, E2, E3)> mergeStreams3<E1, E2, E3>(
+  Stream<E1> stream1, 
+  Stream<E2> stream2,
+  Stream<E3> stream3,
+) {
+  return mergeStreams([stream1, stream2, stream3]).map((list) {
+    var [event1, event2, event3] = list;
+    return (event1, event2, event3);
+  });
+}
+
+Stream<(E1, E2, E3, E4)> mergeStreams4<E1, E2, E3, E4>(
+  Stream<E1> stream1, 
+  Stream<E2> stream2,
+  Stream<E3> stream3,
+  Stream<E4> stream4,
+) {
+  return mergeStreams([stream1, stream2, stream3, stream4]).map((list) {
+    var [event1, event2, event3, event4] = list;
+    return (event1, event2, event3, event4);
+  });
+}
+
+Stream<(E1, E2, E3, E4, E5)> mergeStreams5<E1, E2, E3, E4, E5>(
+  Stream<E1> stream1, 
+  Stream<E2> stream2,
+  Stream<E3> stream3,
+  Stream<E4> stream4,
+  Stream<E5> stream5,
+) {
+  return mergeStreams([stream1, stream2, stream3, stream4, stream5]).map((list) {
+    var [event1, event2, event3, event4, event5] = list;
+    return (event1, event2, event3, event4, event5);
+  });
+}
+
+Stream<(E1, E2, E3, E4, E5, E6)> mergeStreams6<E1, E2, E3, E4, E5, E6>(
+  Stream<E1> stream1,
+  Stream<E2> stream2,
+  Stream<E3> stream3,
+  Stream<E4> stream4,
+  Stream<E5> stream5,
+  Stream<E6> stream6,
+) {
+  return mergeStreams([stream1, stream2, stream3, stream4, stream5, stream6]).map((list) {
+    var [event1, event2, event3, event4, event5, event6] = list;
+    return (event1, event2, event3, event4, event5, event6);
+  });
+}
+
+Stream<(E1, E2, E3, E4, E5, E6, E7)> mergeStreams7<E1, E2, E3, E4, E5, E6, E7>(
+  Stream<E1> stream1,
+  Stream<E2> stream2,
+  Stream<E3> stream3,
+  Stream<E4> stream4,
+  Stream<E5> stream5,
+  Stream<E6> stream6,
+  Stream<E7> stream7,
+) {
+  return mergeStreams([stream1, stream2, stream3, stream4, stream5, stream6, stream7]).map((list) {
+    var [event1, event2, event3, event4, event5, event6, event7] = list;
+    return (event1, event2, event3, event4, event5, event6, event7);
+  });
+}
+
+Stream<(E1, E2, E3, E4, E5, E6, E7, E8)> mergeStreams8<E1, E2, E3, E4, E5, E6, E7, E8>(
+  Stream<E1> stream1,
+  Stream<E2> stream2,
+  Stream<E3> stream3,
+  Stream<E4> stream4,
+  Stream<E5> stream5,
+  Stream<E6> stream6,
+  Stream<E7> stream7,
+  Stream<E8> stream8,
+) {
+  return mergeStreams([stream1, stream2, stream3, stream4, stream5, stream6, stream7, stream8]).map((list) {
+    var [event1, event2, event3, event4, event5, event6, event7, event8] = list;
+    return (event1, event2, event3, event4, event5, event6, event7, event8);
+  });
+}
+
+Stream<(E1, E2, E3, E4, E5, E6, E7, E8, E9)> mergeStreams9<E1, E2, E3, E4, E5, E6, E7, E8, E9>(
+  Stream<E1> stream1,
+  Stream<E2> stream2,
+  Stream<E3> stream3,
+  Stream<E4> stream4,
+  Stream<E5> stream5,
+  Stream<E6> stream6,
+  Stream<E7> stream7,
+  Stream<E8> stream8,
+  Stream<E9> stream9,
+) {
+  return mergeStreams([stream1, stream2, stream3, stream4, stream5, stream6, stream7, stream8, stream9]).map((list) {
+    var [event1, event2, event3, event4, event5, event6, event7, event8, event9] = list;
+    return (event1, event2, event3, event4, event5, event6, event7, event8, event9);
+  });
+}
+
+Stream<(E1, E2, E3, E4, E5, E6, E7, E8, E9, E10)> mergeStreams10<E1, E2, E3, E4, E5, E6, E7, E8, E9, E10>(
+  Stream<E1> stream1,
+  Stream<E2> stream2,
+  Stream<E3> stream3,
+  Stream<E4> stream4,
+  Stream<E5> stream5,
+  Stream<E6> stream6,
+  Stream<E7> stream7,
+  Stream<E8> stream8,
+  Stream<E9> stream9,
+  Stream<E10> stream10,
+) {
+  return mergeStreams([
+    stream1, stream2, stream3, stream4, stream5,
+    stream6, stream7, stream8, stream9, stream10
+  ]).map((list) {
+    var [event1, event2, event3, event4, event5, event6, event7, event8, event9, event10] = list;
+    return (event1, event2, event3, event4, event5, event6, event7, event8, event9, event10);
+  });
+}
+
+
+class _MergeStreamsManager {
+  late final sharedDestController = StreamController<List<dynamic>>(
+    onListen: onListen,
+    onCancel: onCancel,
+    onPause: onPause,
+    onResume: onResume,
+  );
+
+  final _sourceStreams = <Stream<dynamic>>[];
+  final _listenHandlers = <void Function()>[];
+  final _cancelHandlers = <FutureOr<void> Function()>[];
+  final _pauseHandlers = <void Function()>[];
+  final _resumeHandlers = <void Function()>[];
+
+  final _streamState = <Stream, dynamic>{};
+
+  void onListen() {
+    for (var listen in _listenHandlers) {
+      listen();
+    }
+  }
+
+  FutureOr<void> onCancel() async {
+    await Future.wait([
+      for (var cancel in _cancelHandlers)
+        Future.value(cancel())
+    ]);
+    await sharedDestController.closeIfNeeded();
+  }
+
+  void onPause() {
+    for (var pause in _pauseHandlers) {
+      pause();
+    }
+  }
+
+  void onResume() {
+    for (var resume in _resumeHandlers) {
+      resume();
+    }
+  }
+
+  void addHandlers(Stream sourceStream, StreamControllerHandlers handlers) {
+    _sourceStreams.add(sourceStream);
+
+    _listenHandlers.add(handlers.listen);
+    _cancelHandlers.add(handlers.cancel);
+    _pauseHandlers.add(handlers.pause);
+    _resumeHandlers.add(handlers.resume);
+  }
+
+  void removeHandlers(StreamControllerHandlers handlers) {
+    _listenHandlers.remove(handlers.listen);
+    _cancelHandlers.remove(handlers.cancel);
+    _pauseHandlers.remove(handlers.pause);
+    _resumeHandlers.remove(handlers.resume);
+
+    if (_listenHandlers.isEmpty) {
+      sharedDestController.closeIfNeeded();
+    }
+  }
+
+  void updateLatest<SourceT>(Stream<SourceT> sourceStream, SourceT event) {
+    _streamState[sourceStream] = event;
+
+    var allStreamsHaveEmitted = _streamState.length == _sourceStreams.length;
+    if (allStreamsHaveEmitted) {
+      var mergedEvent = _sourceStreams
+        .map((stream) => _streamState[stream]!)
+        .toList();
+      sharedDestController.add(mergedEvent);
+    }
+  }
+}
+
+final class _MergeStreamsTransformer<SourceT> extends StreamLifecycleTransformer<SourceT, dynamic> {
+  final _MergeStreamsManager manager;
+  final int streamIdx;
+  final Stream<SourceT> originalStream;
+
+  _MergeStreamsTransformer(this.manager, {required this.streamIdx, required this.originalStream});
+
+  late final StreamControllerHandlers handlers;
+
+  @override
+  StreamController onBindDestController(Stream<SourceT> sourceStream, StreamControllerHandlers handlers) {
+    manager.addHandlers(sourceStream, handlers);
+    this.handlers = handlers;
+    return manager.sharedDestController;
+  }
+
+  @override
+  FutureOr<StreamSubscription<SourceT>?> destOnCancel(TransformerContext<SourceT, dynamic> context) async {
+    await context.sourceSubscription?.cancel();
+    // manager class is responsible for closing the controller
+    return null;
+  }
+
+  @override
+  void sourceOnData(TransformerContext<SourceT, dynamic> context, SourceT event) {
+    manager.updateLatest(context.sourceStream, event);
+  }
+
+  @override
+  void sourceOnError(TransformerContext<SourceT, dynamic> context, Object error, StackTrace stackTrace) {
+    var errorPacket = (index: streamIdx, stream: originalStream, error: error);
+    context.destController.addError(errorPacket, stackTrace);
+  }
+
+  @override
+  FutureOr<void> sourceOnDone(TransformerContext<SourceT, dynamic> context) {
+    manager.removeHandlers(handlers);
+    // manager class is responsible for closing the controller
+  }
+}
---
+++ test/merge_streams_test.dart
@@ -0,0 +1,457 @@
+import 'dart:async';
+import 'package:scopely/scopely.dart';
+import 'package:scopely/src/merge_streams.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("The default mergeStreams()", () {
+    late StreamController<int> controller1;
+    late StreamController<double> controller2;
+    late StreamController<String?> controller3;
+    late Stream<List<dynamic>> mergedStream;
+    StreamSubscription? mergedStreamSubscription;
+
+    setUp(() {
+      controller1 = StreamController();
+      controller2 = StreamController.broadcast(); // because it can hold "double" (or more) subscriptions! :D
+      controller3 = StreamController();
+      mergedStream = mergeStreams([
+        controller1.stream,
+        controller2.stream,
+        controller3.stream,
+      ]);
+    });
+
+    tearDown(() {
+      controller1.close();
+      controller2.close();
+      controller3.close();
+      mergedStreamSubscription!.cancel(); // tests should assign this if they haven't
+    });
+
+    test("forwards a combined list of values from all streams", () async {
+      var dataCompleter = Completer<List<dynamic>>();
+      mergedStreamSubscription = mergedStream.listen((data) {
+        dataCompleter.complete(data);
+      });
+
+      controller1.add(1);
+      controller2.add(2);
+      controller3.add("3");
+
+      var data = await dataCompleter.future;
+
+      expect(data, [1, 2, "3"]);
+    });
+
+    test("doesn't emit any data until all source streams have emitted once", () async {
+      var dataSentYet = false;
+      mergedStreamSubscription = mergedStream.listen((data) {
+        dataSentYet = true;
+      });
+
+      controller1.add(1);
+      await Future.delayed(Duration.zero);
+
+      expect(dataSentYet, false);
+
+      controller2.add(2);
+      await Future.delayed(Duration.zero);
+
+      expect(dataSentYet, false);
+
+      controller3.add("3");
+      await Future.delayed(Duration.zero);
+
+      expect(dataSentYet, true);
+    });
+
+    test("sends new data when any source streams have emitted new data", () async {
+      var dataReceived = [];
+      mergedStreamSubscription = mergedStream.listen((data) {
+        dataReceived.add(data);
+      });
+
+      controller1.add(1);
+      await Future.delayed(Duration.zero);
+      controller2.add(2);
+      await Future.delayed(Duration.zero);
+      controller3.add("3");
+      await Future.delayed(Duration.zero);
+      controller3.add("4");
+      await Future.delayed(Duration.zero);
+      controller2.add(5);
+      await Future.delayed(Duration.zero);
+
+      expect(dataReceived, [
+        [1, 2, "3"],
+        [1, 2, "4"],
+        [1, 5, "4"],
+      ]);
+    });
+
+    test("errors and stack traces are forwarded", () async {
+      var errors = [];
+      var stackTraces = [];
+      mergedStreamSubscription = mergedStream.listen(null, onError: (error, stackTrace) {
+        errors.add(error);
+        stackTraces.add(stackTrace);
+      });
+
+      var error1 = StateError("first error");
+      controller1.addError(error1);
+      var error2 = {"amIAnError": "totally"};
+      controller2.addError(error2);
+      var error3 = ["this", "is", "a", "completely", "normal", "error"];
+      controller3.addError(error3);
+      await Future.delayed(Duration.zero);
+
+      expect(errors, [
+        (index: 0, stream: controller1.stream, error: error1), 
+        (index: 1, stream: controller2.stream, error: error2), 
+        (index: 2, stream: controller3.stream, error: error3),
+      ]);
+      for (var stackTrace in stackTraces) {
+        expect(stackTrace, isA<StackTrace>());
+      }
+    });
+
+    test("done events are forwarded (when all complete)", () async {
+      var isDone = false;
+      mergedStreamSubscription = mergedStream.listen(null, onDone: () => isDone = true);
+
+      await controller1.close();
+      await Future.delayed(Duration.zero);
+
+      expect(isDone, false);
+
+      await controller2.close();
+      await Future.delayed(Duration.zero);
+
+      expect(isDone, false);
+
+      await controller3.close();
+      await Future.delayed(Duration.zero);
+
+      expect(isDone, true);
+    });
+  });
+
+  group("N-airty mergeStreamsN()", () {
+    late StreamController<int> controller1;
+    late StreamController<int> controller2;
+    late StreamController<int> controller3;
+    late StreamController<int> controller4;
+    late StreamController<int> controller5;
+    late StreamController<int> controller6;
+    late StreamController<int> controller7;
+    late StreamController<int> controller8;
+    late StreamController<int> controller9;
+    late StreamController<int> controller10;
+
+    setUp(() {
+      controller1 = StreamController()
+        ..add(11)
+        ..add(12)
+        ..add(13)
+        ..close();
+      controller2 = StreamController()
+        ..add(21)
+        ..add(22)
+        ..add(23)
+        ..close();
+      controller3 = StreamController()
+        ..add(31)
+        ..add(32)
+        ..add(33)
+        ..close();
+      controller4 = StreamController()
+        ..add(41)
+        ..add(42)
+        ..add(43)
+        ..close();
+      controller5 = StreamController()
+        ..add(51)
+        ..add(52)
+        ..add(53)
+        ..close();
+      controller6 = StreamController()
+        ..add(61)
+        ..add(62)
+        ..add(63)
+        ..close();
+      controller7 = StreamController()
+        ..add(71)
+        ..add(72)
+        ..add(73)
+        ..close();
+      controller8 = StreamController()
+        ..add(81)
+        ..add(82)
+        ..add(83)
+        ..close();
+      controller9 = StreamController()
+        ..add(91)
+        ..add(92)
+        ..add(93)
+        ..close();
+      controller10 = StreamController()
+        ..add(101)
+        ..add(102)
+        ..add(103)
+        ..close();
+    });
+
+    test("mergeStreams2() returns its record correctly", () async {
+      var stream = mergeStreams2(
+        controller1.stream,
+        controller2.stream,
+      );
+      var results = await stream.toList();
+
+      expect(results, [
+        (11, 21),
+        (12, 21),
+        (12, 22),
+        (13, 22),
+        (13, 23),
+      ]);
+    });
+
+    test("mergeStreams3() returns its record correctly", () async {
+      var stream = mergeStreams3(
+        controller1.stream,
+        controller2.stream,
+        controller3.stream,
+      );
+      var results = await stream.toList();
+
+      expect(results, [
+        (11, 21, 31),
+        (12, 21, 31),
+        (12, 22, 31),
+        (12, 22, 32),
+        (13, 22, 32),
+        (13, 23, 32),
+        (13, 23, 33),
+      ]);
+    });
+
+    test("mergeStreams4() returns its record correctly", () async {
+      var stream = mergeStreams4(
+        controller1.stream,
+        controller2.stream,
+        controller3.stream,
+        controller4.stream,
+      );
+      var results = await stream.toList();
+
+      expect(results, [
+        (11, 21, 31, 41),
+        (12, 21, 31, 41),
+        (12, 22, 31, 41),
+        (12, 22, 32, 41),
+        (12, 22, 32, 42),
+        (13, 22, 32, 42),
+        (13, 23, 32, 42),
+        (13, 23, 33, 42),
+        (13, 23, 33, 43),
+      ]);
+    });
+
+    test("mergeStreams5() returns its record correctly", () async {
+      var stream = mergeStreams5(
+        controller1.stream,
+        controller2.stream,
+        controller3.stream,
+        controller4.stream,
+        controller5.stream,
+      );
+      var results = await stream.toList();
+
+      expect(results, [
+        (11, 21, 31, 41, 51),
+        (12, 21, 31, 41, 51),
+        (12, 22, 31, 41, 51),
+        (12, 22, 32, 41, 51),
+        (12, 22, 32, 42, 51),
+        (12, 22, 32, 42, 52),
+        (13, 22, 32, 42, 52),
+        (13, 23, 32, 42, 52),
+        (13, 23, 33, 42, 52),
+        (13, 23, 33, 43, 52),
+        (13, 23, 33, 43, 53),
+      ]);
+    });
+
+    test("mergeStreams6() returns its record correctly", () async {
+      var stream = mergeStreams6(
+        controller1.stream,
+        controller2.stream,
+        controller3.stream,
+        controller4.stream,
+        controller5.stream,
+        controller6.stream,
+      );
+      var results = await stream.toList();
+
+      expect(results, [
+        (11, 21, 31, 41, 51, 61),
+        (12, 21, 31, 41, 51, 61),
+        (12, 22, 31, 41, 51, 61),
+        (12, 22, 32, 41, 51, 61),
+        (12, 22, 32, 42, 51, 61),
+        (12, 22, 32, 42, 52, 61),
+        (12, 22, 32, 42, 52, 62),
+        (13, 22, 32, 42, 52, 62),
+        (13, 23, 32, 42, 52, 62),
+        (13, 23, 33, 42, 52, 62),
+        (13, 23, 33, 43, 52, 62),
+        (13, 23, 33, 43, 53, 62),
+        (13, 23, 33, 43, 53, 63),
+      ]);
+    });
+
+    test("mergeStreams7() returns its record correctly", () async {
+      var stream = mergeStreams7(
+        controller1.stream,
+        controller2.stream,
+        controller3.stream,
+        controller4.stream,
+        controller5.stream,
+        controller6.stream,
+        controller7.stream,
+      );
+      var results = await stream.toList();
+
+      expect(results, [
+        (11, 21, 31, 41, 51, 61, 71),
+        (12, 21, 31, 41, 51, 61, 71),
+        (12, 22, 31, 41, 51, 61, 71),
+        (12, 22, 32, 41, 51, 61, 71),
+        (12, 22, 32, 42, 51, 61, 71),
+        (12, 22, 32, 42, 52, 61, 71),
+        (12, 22, 32, 42, 52, 62, 71),
+        (12, 22, 32, 42, 52, 62, 72),
+        (13, 22, 32, 42, 52, 62, 72),
+        (13, 23, 32, 42, 52, 62, 72),
+        (13, 23, 33, 42, 52, 62, 72),
+        (13, 23, 33, 43, 52, 62, 72),
+        (13, 23, 33, 43, 53, 62, 72),
+        (13, 23, 33, 43, 53, 63, 72),
+        (13, 23, 33, 43, 53, 63, 73),
+      ]);
+    });
+
+    test("mergeStreams8() returns its record correctly", () async {
+      var stream = mergeStreams8(
+        controller1.stream,
+        controller2.stream,
+        controller3.stream,
+        controller4.stream,
+        controller5.stream,
+        controller6.stream,
+        controller7.stream,
+        controller8.stream,
+      );
+      var results = await stream.toList();
+
+      expect(results, [
+        (11, 21, 31, 41, 51, 61, 71, 81),
+        (12, 21, 31, 41, 51, 61, 71, 81),
+        (12, 22, 31, 41, 51, 61, 71, 81),
+        (12, 22, 32, 41, 51, 61, 71, 81),
+        (12, 22, 32, 42, 51, 61, 71, 81),
+        (12, 22, 32, 42, 52, 61, 71, 81),
+        (12, 22, 32, 42, 52, 62, 71, 81),
+        (12, 22, 32, 42, 52, 62, 72, 81),
+        (12, 22, 32, 42, 52, 62, 72, 82),
+        (13, 22, 32, 42, 52, 62, 72, 82),
+        (13, 23, 32, 42, 52, 62, 72, 82),
+        (13, 23, 33, 42, 52, 62, 72, 82),
+        (13, 23, 33, 43, 52, 62, 72, 82),
+        (13, 23, 33, 43, 53, 62, 72, 82),
+        (13, 23, 33, 43, 53, 63, 72, 82),
+        (13, 23, 33, 43, 53, 63, 73, 82),
+        (13, 23, 33, 43, 53, 63, 73, 83),
+      ]);
+    });
+
+    test("mergeStreams9() returns its record correctly", () async {
+      var stream = mergeStreams9(
+        controller1.stream,
+        controller2.stream,
+        controller3.stream,
+        controller4.stream,
+        controller5.stream,
+        controller6.stream,
+        controller7.stream,
+        controller8.stream,
+        controller9.stream,
+      );
+      var results = await stream.toList();
+
+      expect(results, [
+        (11, 21, 31, 41, 51, 61, 71, 81, 91),
+        (12, 21, 31, 41, 51, 61, 71, 81, 91),
+        (12, 22, 31, 41, 51, 61, 71, 81, 91),
+        (12, 22, 32, 41, 51, 61, 71, 81, 91),
+        (12, 22, 32, 42, 51, 61, 71, 81, 91),
+        (12, 22, 32, 42, 52, 61, 71, 81, 91),
+        (12, 22, 32, 42, 52, 62, 71, 81, 91),
+        (12, 22, 32, 42, 52, 62, 72, 81, 91),
+        (12, 22, 32, 42, 52, 62, 72, 82, 91),
+        (12, 22, 32, 42, 52, 62, 72, 82, 92),
+        (13, 22, 32, 42, 52, 62, 72, 82, 92),
+        (13, 23, 32, 42, 52, 62, 72, 82, 92),
+        (13, 23, 33, 42, 52, 62, 72, 82, 92),
+        (13, 23, 33, 43, 52, 62, 72, 82, 92),
+        (13, 23, 33, 43, 53, 62, 72, 82, 92),
+        (13, 23, 33, 43, 53, 63, 72, 82, 92),
+        (13, 23, 33, 43, 53, 63, 73, 82, 92),
+        (13, 23, 33, 43, 53, 63, 73, 83, 92),
+        (13, 23, 33, 43, 53, 63, 73, 83, 93),
+      ]);
+    });
+
+    test("mergeStreams10() returns its record correctly", () async {
+      var stream = mergeStreams10(
+        controller1.stream,
+        controller2.stream,
+        controller3.stream,
+        controller4.stream,
+        controller5.stream,
+        controller6.stream,
+        controller7.stream,
+        controller8.stream,
+        controller9.stream,
+        controller10.stream,
+      );
+      var results = await stream.toList();
+
+      expect(results, [
+        (11, 21, 31, 41, 51, 61, 71, 81, 91, 101),
+        (12, 21, 31, 41, 51, 61, 71, 81, 91, 101),
+        (12, 22, 31, 41, 51, 61, 71, 81, 91, 101),
+        (12, 22, 32, 41, 51, 61, 71, 81, 91, 101),
+        (12, 22, 32, 42, 51, 61, 71, 81, 91, 101),
+        (12, 22, 32, 42, 52, 61, 71, 81, 91, 101),
+        (12, 22, 32, 42, 52, 62, 71, 81, 91, 101),
+        (12, 22, 32, 42, 52, 62, 72, 81, 91, 101),
+        (12, 22, 32, 42, 52, 62, 72, 82, 91, 101),
+        (12, 22, 32, 42, 52, 62, 72, 82, 92, 101),
+        (12, 22, 32, 42, 52, 62, 72, 82, 92, 102),
+        (13, 22, 32, 42, 52, 62, 72, 82, 92, 102),
+        (13, 23, 32, 42, 52, 62, 72, 82, 92, 102),
+        (13, 23, 33, 42, 52, 62, 72, 82, 92, 102),
+        (13, 23, 33, 43, 52, 62, 72, 82, 92, 102),
+        (13, 23, 33, 43, 53, 62, 72, 82, 92, 102),
+        (13, 23, 33, 43, 53, 63, 72, 82, 92, 102),
+        (13, 23, 33, 43, 53, 63, 73, 82, 92, 102),
+        (13, 23, 33, 43, 53, 63, 73, 83, 92, 102),
+        (13, 23, 33, 43, 53, 63, 73, 83, 93, 102),
+        (13, 23, 33, 43, 53, 63, 73, 83, 93, 103),
+      ]);
+    });
+  });
+}
