0601735a05373d06204efcb4791e04a6547f0901
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2025-06-13 23:10:32 -0600

    Add documentation for `asFutures()`

--- lib/src/stream_as_futures.dart
+++ lib/src/stream_as_futures.dart
@@ -2,34 +2,60 @@ import 'dart:async';
 
 import 'package:scopely/scopely.dart';
 
-// TODO: modify and use this docstring for asFutures()
-  /// Creates a new stream that listens to this one, transforming all events into [Future]s
-  /// that can be used intuitively in `await for` loops.
+
+/// An extension that provides an [asFutures] transformation method for [Stream]s.
+extension StreamAsFutures<EventT> on Stream<EventT> {
+  /// Returns a stream that wraps every result in its own [Future]. This can be used
+  /// to improve exception handling in `await for` loops.
   /// 
-  /// In other words, the futures emitted by the resulting stream can be used similar to
-  /// the callback parameters available from [Stream.listen]:
+  /// For example, let's say events are being received from a stream like this:
+  /// 
+  /// ```dart
+  /// await for (var event in stream) {
+  ///   // ...
+  /// }
+  /// ```
+  /// 
+  /// If the stream never sends errors, then you're good to go! However, if the stream
+  /// *does* emit errors, there's no way to catch them without breaking out of the loop.
+  /// 
+  /// ```dart
+  /// try {
+  ///   await for (var event in stream) {
+  ///     try {
+  ///       // process `event`...
+  ///     } catch (errorInsideLoop) {
+  ///       // uh oh! this only catches errors from processing `event` *after*
+  ///       // it was emitted, not errors emitted from the stream itself!
+  ///     }
+  ///   }
+  /// } catch (errorOutsideLoop) {
+  ///   // this catches errors from the stream, but it's outside the loop!
+  /// }
+  /// ```
+  /// 
+  /// Doesn't it feel like you *should* be able to do this? Well, look no further!
+  /// This is exactly the problem this method was designed to solve!
+  /// 
   /// ```dart
   /// try {
   ///   await for (var eventFuture in stream.asFutures()) {
   ///     try {
-  ///       // event received, like `listen(onData: ...)`
-  ///       var event = await eventFuture
-  ///     } catch (error) {
-  ///       // error received, like `listen(onError: ...)`
+  ///       var event = await eventFuture;
+  ///       // process `event`...
+  ///     } on Exception catch (errorInsideLoop) {
+  ///       // nice! this catches errors emitted from the stream now!
   ///     }
   ///   }
-  ///   // stream closed, like `listen(onDone: ...)`
-  /// } catch (error) {
-  ///   // error thrown when calling `stream.listen()`,
-  ///   // or like `listen(onError: ..., cancelOnError: true)`
-  ///   // if there's no inner `try` block
+  /// } on Error catch (errorOutsideLoop) {
+  ///   // this can catch its own set of errors that should still break the loop
   /// }
   /// ```
-
-extension StreamAsFutures<EventT> on Stream<EventT> {
   Stream<Future<EventT>> asFutures() => transform(_AsFuturesTransformer());
 }
 
+/// A [StreamLifecycleTransformer] that facilitates transforming stream errors into
+/// successful emissions of failed futures.
 final class _AsFuturesTransformer<EventT> extends StreamLifecycleTransformer<EventT, Future<EventT>> {
   @override
   void sourceOnData(TransformerContext<EventT, Future<EventT>> context, EventT event) {
